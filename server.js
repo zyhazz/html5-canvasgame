9d8f8ecc02d526fe129775f5cd44d213f7965e29
Diego Carvalho <zyhazz@gmail.com>
2025-05-27 23:44:41 -0300

    Add nodemon for development and update file paths in server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const io = new Server(server);
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/public/index.html');
 });
 
 let players = {}, count = 0;
